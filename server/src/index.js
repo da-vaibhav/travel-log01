27beb8568ad3ade15e2bf027224296d5b2d64ac0
da-vaibhav <da-vaibhav@users.noreply.github.com>
2020-02-04 02:30:39 +0530

    api and models

--- server/src/index.js
+++ server/src/index.js
@@ -3,19 +3,31 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 const middlewares = require('./middlewares');
+const mongoose = require('mongoose');
+
+const logs = require('./api/logs');
+
+require('dotenv').config();
+
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const app = express();
 const port = process.env.PORT || 1337;
 
-app.use(morgan());
+app.use(morgan('common'));
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN
+}));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({
     message: 'Hello world!'
   });
 });
+
+app.use('/api/logs', logs);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler)
