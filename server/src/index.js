b824b7804fe157c796848ec3db0a426f141fbac4
da-vaibhav <da-vaibhav@users.noreply.github.com>
2020-02-03 17:09:29 +0530

    moved middlewares to a new file

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const middlewares = require('./middlewares');
 
 const app = express();
 const port = process.env.PORT || 1337;
@@ -16,20 +17,8 @@ app.get('/', (req, res) => {
   });
 });
 
-app.use((req, res, next) => {
-  const error = new Error(`Not Found - ${req.originalUrl}`);
-  res.status(404);
-  next(error);
-});
-
-app.use((error, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-  res.json({
-    message: error.message,
-    stack: process.env.NODE_ENV === 'production' ? '🙃' : error.stack
-  });
-});
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler)
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:1337`);
